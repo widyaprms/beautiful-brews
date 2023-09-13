73f683284f2496cd34cf90ea7886b9148792550a
widyaprms <arini.widya@ui.ac.id>
2023-09-13 09:42:14 +0700

    Memperbarui berkas models.py

--- main/models.py
+++ main/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255) 
     amount = models.IntegerField() 
     description = models.TextField() 
+
+
+# function untuk memeriksa stock produk BeautyBrews Co.
+def stock_checking(self):
+    output = ""
+    if self.amount > 0:
+        output = "Stock of " + self.name + " is currently available: " + self.amount + " pcs."
+    else:
+        output = "Stock of " + self.name + " is currently not available."
+    return output
--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 from django.test import TestCase, Client
+from models import Product
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -12,3 +13,16 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+class selfTest(TestCase):
+    def ProductSetUp(self):
+        Product.objects.create(name = "Tiny Cup", amount = "56", description = "blue butterflies edition")
+        Product.objects.create(name = "Not So Tiny Cup", amount = "13", description = "halloween edition")
+        Product.objects.create(name = "Very Not Tiny Cup", amount = "0", description = "cats edition")
+
+    def product_check_stock(self):
+        tiny_cup = Product.objects.get(name = "Tiny Cup")
+        self.assertEqual(tiny_cup.stock_checking(), "Stock of Tiny Cup is currently available: 56 pcs.")
+        very_not_tiny = Product.objects.get(description = "cats edition")
+        self.assertEqual(very_not_tiny.stock_checking(), "Stock of Very Not Tiny Cup is currently not available.")
+
