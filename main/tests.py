99f6a4fb1b25d2c29b098c4b6024c0568b48d8b8
widyaprms <arini.widya@ui.ac.id>
2023-11-21 23:40:24 +0700

    Menambahkan konfigurasi deployment

--- main/tests.py
+++ main/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 from django.test import TestCase, Client
-#from main.models import Item
+
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
